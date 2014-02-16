a3a3d2e3f67050ffbae5a08d4894c09171cd7c8e
erikcas <erikcas1972@gmail.com>
2014-02-16 18:01:09 +0100

    Revert "test libGLESv2S3D_adreno200.so blob"
    This reverts commit edb52022f7c85ee82f8dcb482f17859fd0c5dd2e.

--- msm7x27a-common/blobs.mk
+++ msm7x27a-common/blobs.mk
@@ -64,7 +64,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/msm7x27a-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
-    vendor/samsung/msm7x27a-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
@@ -75,7 +74,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/msm7x27a-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
     vendor/samsung/msm7x27a-common/proprietary/lib/libsc-a3xx.so:system/lib/libsc-a3xx.so
-
 
 ## GPU firmware
 PRODUCT_COPY_FILES += \
